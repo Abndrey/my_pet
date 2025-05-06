d002732a92beededd53fb56b704e062220f50606
abnerolimor <abner.oliveira.moraes@escola.pr.gov.br>
2025-05-06 08:59:06 -0300

    primeiro commit

---
+++ src/main/java/com/my_pet/modelo/Endereco.java
@@ -0,0 +1,36 @@
+package main.java.com.my_pet.modelo;
+
+public class Endereco { 
+    private String rua;
+    private String num;
+    private String bairro;
+
+
+
+//---------------------------------------Encapsulamento rua ----------------------------------------------//
+public void setRua(String rua) {
+    this.rua = rua;
+}
+
+public String getRua() {
+    return this.rua;
+}
+
+//---------------------------------------Encapsulamento num ----------------------------------------------//
+public void setNum(String num) {
+    this.num = num;
+}
+
+public String getNum() {
+    return this.num;
+}
+
+//---------------------------------------Encapsulamento bairro ----------------------------------------------//
+public void setBairro(String bairro) {
+    this.bairro = bairro;
+}
+
+public String getBairro() {
+    return this.bairro;
+}
+}
