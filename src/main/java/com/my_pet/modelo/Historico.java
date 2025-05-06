33f89c5ca19b52f843a2f08745cbbea049a1b7ff
comininick <nicolle.jerke@escola.pr.gov.br>
2025-05-06 11:17:22 -0300

    construtor feito

--- src/main/java/com/my_pet/modelo/Historico.java
+++ src/main/java/com/my_pet/modelo/Historico.java
@@ -2,5 +2,14 @@ public class Historico {
    
     public String dataVacina;
     public String proximaVacina;
+ 
+    //--------------------------------------- construtor ----------------------------------------------//
+  public Historico(){
 
+  }
+
+  public Historico( String dataVacina, String proximaVacina){
+      this.dataVacina = dataVacina;
+      this.proximaVacina = proximaVacina;
+     }
 }
--- src/main/java/com/my_pet/modelo/Vacina.java
+++ src/main/java/com/my_pet/modelo/Vacina.java
@@ -1,4 +1,17 @@
 public class Vacina {
     
     public String nomeVacina;
-}
+    
+                             
+     //--------------------------------------- construtor ----------------------------------------------//
+     public Vacina(){
+
+     }
+ 
+     public Vacina( String nomeVacina){
+         this.nomeVacina = nomeVacina;
+        }
+
+    }
+ 
+
